f8c2580b2300d1e8984e16b3c6ee2d30f0de95e4
gonzalo <garcaiglesias@danielcastelao.org>
2023-10-25 08:23:38 +0200

    EjercicioClase

--- Contador/app/src/main/java/com/dam/contador/MainActivity.kt
+++ Contador/app/src/main/java/com/dam/contador/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
         val myViewModel: MyViewModel= MyViewModel()
         setContent {
             ContadorTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
